5226d8eb394b4da2feb8480d25219f186471df1f
Cyliann <maksymilian@cych.eu>
2024-10-14 17:15:07 +0200

    Add intersection distance and float color.

--- frag.glsl
+++ frag.glsl
@@ -3,24 +3,27 @@ out vec4 frag_color;
 
 uniform vec2 uSize;
 
-int raySphereIntersection(vec3 rayOrigin, vec3 rayDirection, float radius) {
-    int hit = 0;
+vec3 raySphereIntersection(vec3 rayOrigin, vec3 rayDirection, float radius) {
+    vec3 color = vec3(0);
     float a = dot(rayDirection, rayDirection);
     float b = 2.0 * dot(rayOrigin, rayDirection);
     float c = dot(rayOrigin, rayOrigin) - pow(radius, 2);
     float discriminant = pow(b, 2) - 4 * a * c;
 
-    if (discriminant >= 0) {
-        hit = 1;
+    if (discriminant < 0) {
+        return color;
     }
 
-    return hit;
+    float d = (-b - sqrt(discriminant)) / (2 * a);
+    vec3 hitPoint = rayOrigin + rayDirection * d;
+    color = normalize(hitPoint) * .5 + .5;
+    return color;
 }
 
 void main() {
     vec2 uv = (gl_FragCoord.xy * 2.0 - uSize) / uSize.y;
-    vec3 rayOrigin = vec3(0, 0, 5);
-    vec3 rayDirection = vec3(uv, 1);
+    vec3 rayOrigin = vec3(0, 0, 2);
+    vec3 rayDirection = normalize(vec3(uv, -1));
 
-    frag_color = vec4(raySphereIntersection(rayOrigin, rayDirection, 2), 0, 0, 1);
+    frag_color = vec4(raySphereIntersection(rayOrigin, rayDirection, .7), 1);
 }
