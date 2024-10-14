9cb1d1433d92dd235734a63ee16b7cfc96b0023a
Cyliann <maksymilian@cych.eu>
2024-10-14 17:23:26 +0200

    Add shadows

--- frag.glsl
+++ frag.glsl
@@ -3,7 +3,7 @@ out vec4 frag_color;
 
 uniform vec2 uSize;
 
-vec3 raySphereIntersection(vec3 rayOrigin, vec3 rayDirection, float radius) {
+vec3 raySphereIntersection(vec3 rayOrigin, vec3 rayDirection, float radius, vec3 lightDirection) {
     vec3 color = vec3(0);
     float a = dot(rayDirection, rayDirection);
     float b = 2.0 * dot(rayOrigin, rayDirection);
@@ -16,7 +16,10 @@ vec3 raySphereIntersection(vec3 rayOrigin, vec3 rayDirection, float radius) {
 
     float d = (-b - sqrt(discriminant)) / (2 * a);
     vec3 hitPoint = rayOrigin + rayDirection * d;
-    color = normalize(hitPoint) * .5 + .5;
+    vec3 normal = normalize(hitPoint);
+    float lighting = max(dot(normal, -lightDirection), 0);
+
+    color = vec3(1, 0, 1) * lighting;
     return color;
 }
 
@@ -24,6 +27,7 @@ void main() {
     vec2 uv = (gl_FragCoord.xy * 2.0 - uSize) / uSize.y;
     vec3 rayOrigin = vec3(0, 0, 2);
     vec3 rayDirection = normalize(vec3(uv, -1));
+    vec3 lightDirection = normalize(vec3(-1));
 
-    frag_color = vec4(raySphereIntersection(rayOrigin, rayDirection, .7), 1);
+    frag_color = vec4(raySphereIntersection(rayOrigin, rayDirection, .7, lightDirection), 1);
 }
