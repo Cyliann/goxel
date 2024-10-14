0f80d2e9375976c1425fa21a18707818f96d7f42
Cyliann <maksymilian@cych.eu>
2024-10-14 16:37:53 +0200

    Rework ray-sphere intersection function. Add rayOrigin and rayDirection

--- frag.glsl
+++ frag.glsl
@@ -3,10 +3,24 @@ out vec4 frag_color;
 
 uniform vec2 uSize;
 
-bool circleIntersection(in vec2 uv) {
-    return length(uv) < .5;
+int raySphereIntersection(vec3 rayOrigin, vec3 rayDirection, float radius) {
+    int hit = 0;
+    float a = dot(rayDirection, rayDirection);
+    float b = 2.0 * dot(rayOrigin, rayDirection);
+    float c = dot(rayOrigin, rayOrigin) - pow(radius, 2);
+    float discriminant = pow(b, 2) - 4 * a * c;
+
+    if (discriminant >= 0) {
+        hit = 1;
+    }
+
+    return hit;
 }
+
 void main() {
     vec2 uv = (gl_FragCoord.xy * 2.0 - uSize) / uSize.y;
-    frag_color = vec4(float(circleIntersection(uv)), 0, 0, 1);
+    vec3 rayOrigin = vec3(0, 0, 5);
+    vec3 rayDirection = vec3(uv, 1);
+
+    frag_color = vec4(raySphereIntersection(rayOrigin, rayDirection, 2), 0, 0, 1);
 }
