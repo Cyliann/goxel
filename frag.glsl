65d1b8b6fc12202350c7975e5d2b7cb17f62a99f
Cyliann <maksymilian@cych.eu>
2024-10-14 15:56:02 +0200

    Test ray-sphere intersection

--- frag.glsl
+++ frag.glsl
@@ -3,7 +3,10 @@ out vec4 frag_color;
 
 uniform vec2 uSize;
 
+bool circleIntersection(in vec2 uv) {
+    return length(uv) < .5;
+}
 void main() {
-    vec2 color = gl_FragCoord.xy / uSize;
-    frag_color = vec4(color.xy, pow(1 - color.x * color.y, 2), 1);
+    vec2 uv = (gl_FragCoord.xy * 2.0 - uSize) / uSize.y;
+    frag_color = vec4(float(circleIntersection(uv)), 0, 0, 1);
 }
