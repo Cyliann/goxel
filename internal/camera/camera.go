b0b39a9fa5fad90dd12ce2e2548cccc4281ca965
Cyliann <maksymilian@cych.eu>
2024-10-25 21:51:13 +0200

    Refactor movement speed

--- internal/camera/camera.go
+++ internal/camera/camera.go
@@ -3,6 +3,7 @@ package camera
 import (
 	"math"
 
+	"github.com/charmbracelet/log"
 	"github.com/go-gl/glfw/v3.3/glfw"
 	"github.com/go-gl/mathgl/mgl32"
 )
@@ -25,12 +26,12 @@ func New(window *glfw.Window) Camera {
 	window.SetInputMode(glfw.CursorMode, glfw.CursorDisabled)
 	x, y := window.GetCursorPos()
 	return Camera{
-		mgl32.Vec3{0, 0, 2},                // pos
+		mgl32.Vec3{0, 0, 3},                // pos
 		mgl32.Vec3{0, 0, -1},               // direction
 		mgl32.Vec2{float32(x), float32(y)}, // mous pos
-		0,                                  // pitch
-		0,                                  // yaw
-		90,                                 // fov
+		0,                                  // pitchDelta
+		0,                                  // yawDelta
+		60,                                 // fov
 		1,                                  // nearclip
 		100,                                // farclip
 		mgl32.Ident2().Mat4(),
@@ -40,14 +41,15 @@ func New(window *glfw.Window) Camera {
 
 // Handles mouse and keyboard input. Modifies camera fields
 func (self *Camera) HandleInput(window *glfw.Window) bool {
-	var speed float32 = 0.2
+	var keyboardSpeed float32 = 0.1
+	var mouseSpeed float32 = 0.01
 	up_dir := mgl32.Vec3{0, 1, 0}
 	right_dir := self.Direction.Cross(up_dir)
 
 	shouldUpdate := false
 	x, y := window.GetCursorPos()
-	self.yawDelta = self.MousePos[0] - float32(x)
-	self.pitchDelta = self.MousePos[1] - float32(y)
+	self.yawDelta = (float32(x) - self.MousePos[0]) * mouseSpeed
+	self.pitchDelta = (float32(y) - self.MousePos[1]) * mouseSpeed
 
 	if self.yawDelta != 0 {
 		self.MousePos[0] = float32(x)
@@ -61,37 +63,37 @@ func (self *Camera) HandleInput(window *glfw.Window) bool {
 
 	// Move forward
 	if window.GetKey(glfw.KeyW) == glfw.Press || window.GetKey(glfw.KeyUp) == glfw.Press {
-		self.Pos = self.Pos.Add(self.Direction.Mul(speed))
+		self.Pos = self.Pos.Add(self.Direction.Mul(keyboardSpeed))
 		shouldUpdate = true
 	}
 
 	// Move back
 	if window.GetKey(glfw.KeyS) == glfw.Press || window.GetKey(glfw.KeyDown) == glfw.Press {
-		self.Pos = self.Pos.Add(self.Direction.Mul(-speed))
+		self.Pos = self.Pos.Add(self.Direction.Mul(-keyboardSpeed))
 		shouldUpdate = true
 	}
 
 	// Move right
 	if window.GetKey(glfw.KeyD) == glfw.Press || window.GetKey(glfw.KeyRight) == glfw.Press {
-		self.Pos = self.Pos.Add(right_dir.Mul(speed))
+		self.Pos = self.Pos.Add(right_dir.Mul(keyboardSpeed))
 		shouldUpdate = true
 	}
 
 	// Move left
 	if window.GetKey(glfw.KeyA) == glfw.Press || window.GetKey(glfw.KeyLeft) == glfw.Press {
-		self.Pos = self.Pos.Add(right_dir.Mul(-speed))
+		self.Pos = self.Pos.Add(right_dir.Mul(-keyboardSpeed))
 		shouldUpdate = true
 	}
 
 	// Move up
 	if window.GetKey(glfw.KeySpace) == glfw.Press {
-		self.Pos = self.Pos.Add(up_dir.Mul(speed))
+		self.Pos = self.Pos.Add(up_dir.Mul(keyboardSpeed))
 		shouldUpdate = true
 	}
 
 	// Move down
 	if window.GetKey(glfw.KeyLeftShift) == glfw.Press {
-		self.Pos = self.Pos.Add(up_dir.Mul(-speed))
+		self.Pos = self.Pos.Add(up_dir.Mul(-keyboardSpeed))
 		shouldUpdate = true
 	}
 
@@ -101,13 +103,12 @@ func (self *Camera) HandleInput(window *glfw.Window) bool {
 func (self *Camera) Update(window *glfw.Window) {
 	up_dir := mgl32.Vec3{0, 1, 0}
 	right_dir := self.Direction.Cross(up_dir)
-	var speed float32 = 0.01
 
 	// Create quaternion for pitch (rotation around the right axis)
-	quatPitch := AngleAxis(self.pitchDelta*speed, right_dir)
+	quatPitch := AngleAxis(-self.pitchDelta, right_dir)
 
 	// Create quaternion for yaw (rotation around the up axis, which is (0, 1, 0))
-	quatYaw := AngleAxis(self.yawDelta*speed, mgl32.Vec3{0, 1, 0})
+	quatYaw := AngleAxis(-self.yawDelta, mgl32.Vec3{0, 1, 0})
 
 	// Normalize the resulting quaternion
 	quat := quatPitch.Mul(quatYaw).Normalize()
