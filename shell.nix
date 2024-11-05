6653f51f8682ed477f8f9eb328e0636047fbb656
Maksymilian Cych <maksymilian@cych.eu>
2024-11-06 00:08:20 +0100

    Rewrite nix-shell

--- shell.nix
+++ shell.nix
@@ -1,12 +1,13 @@
-with import <nixpkgs> {};
-  stdenv.mkDerivation {
-    name = "goxel";
-    buildInputs = [
-      wayland
-      libxkbcommon
-      libGL
-      pkg-config
-      glfw-wayland
-      xorg.libX11
-    ];
-  }
+{pkgs ? import <nixpkgs> {}}:
+pkgs.mkShell {
+  packages = with pkgs; [
+    wayland
+    libxkbcommon
+    libGL
+    pkg-config
+    glfw-wayland
+    xorg.libX11
+  ];
+
+  shellHook = "zsh";
+}
